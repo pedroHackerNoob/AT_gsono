18a5f84ce0875a67afac5cded13b66b0122b5ef8
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-07-28 22:34:49 -0700

    updated enviroment.js

--- cypress/e2e/enviroment.js
+++ cypress/e2e/enviroment.js
@@ -1,8 +1,8 @@
 export const url = 'http://localhost:5173/register'
-export const name = "troyano";
-export const logo = "https://example.com/"
-export const location = "https://example.com/"
-export const face = "https://example.com/"
-export const insta = "https://example.com/"
+export const name = "be a man";
+export const logo = "https://www.facebook.com/photo/?fbid=181272244281437&set=a.181272217614773"
+export const location = "https://maps.app.goo.gl/FMASCVPzoehdbqYc8"
+export const face = "https://www.facebook.com/bostonbeaman"
+export const insta = "https://www.instagram.com/bostonbeaman?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
 export const desk = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
-export const chain = 'servicio'
+export const chain = 'experiencia'
